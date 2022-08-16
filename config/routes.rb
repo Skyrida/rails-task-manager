559448c2f4befffc183242fcc7a9bf6b9e2204bb
Alex Rich <alexanderrich28@gmail.com>
2022-08-16 14:56:09 +0800

    Methods and views updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'tasks#index'
+
+  # create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create', as: :create_task
+
+  # read
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update', as: :update_task
+
+  # delete
+  delete 'tasks/:id', to: 'tasks#delete', as: :delete_task
 end
